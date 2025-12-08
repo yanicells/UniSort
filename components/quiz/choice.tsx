99957301e194bbb6e609e1f3c1b57d4432425282
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2025-12-09 00:25:23 +0800

    add functions

--- components/quiz/choice.tsx
+++ components/quiz/choice.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { useState } from "react";
+
 import { Button } from "../ui/button";
 
 export default function Choice({
   choices,
+  setSelectedChoice,
+  selectedChoice,
 }: {
   choices: Array<{
     text: string;
@@ -10,12 +15,31 @@ export default function Choice({
     up: number;
     ust: number;
   }>;
+  setSelectedChoice: (choice: string) => void;
+  selectedChoice: string | null;
 }) {
+  const [score, setScore] = useState<{
+    admu: number;
+    dlsu: number;
+    up: number;
+    ust: number;
+  }>({ admu: 0, dlsu: 0, up: 0, ust: 0 });
+
   return (
     <>
       {choices.map((choice) => (
         <div key={choice.text}>
-          <Button>{choice.text}</Button>
+          {selectedChoice === choice.text ? (
+            <strong>{choice.text}</strong>
+          ) : (
+            <Button
+              onClick={() => {
+                setSelectedChoice(choice.text);
+              }}
+            >
+              {choice.text}
+            </Button>
+          )}
         </div>
       ))}
     </>
--- components/quiz/quiz-view.tsx
+++ components/quiz/quiz-view.tsx
@@ -8,6 +8,18 @@ import Choice from "@/components/quiz/choice";
 
 export default function QuizView() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+  const [selectionMessage, setSelectionMessage] = useState("");
+
+  function handleNext() {
+    if (selectedChoice !== null) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelectedChoice(null);
+      setSelectionMessage("");
+    } else{
+        setSelectionMessage("Please select an answer before proceeding.");
+    }
+  }
 
   return (
     <div>
@@ -15,15 +27,17 @@ export default function QuizView() {
         question={questions.questions[currentQuestionIndex].question}
       ></Question>
       <Choice
+        selectedChoice={selectedChoice}
+        setSelectedChoice={setSelectedChoice}
         choices={questions.questions[currentQuestionIndex].choices}
       ></Choice>
       <Button
-        onClick={() => {
-          setCurrentQuestionIndex(currentQuestionIndex + 1);
-        }}
+        onClick={handleNext}
       >
         Next
       </Button>
+      <p>{selectionMessage}</p>
     </div>
   );
 }
+
