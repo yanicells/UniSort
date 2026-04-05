0121c13755f8cde15f7cd3e77547c536e065c5dc
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-06 00:35:20 +0800

    fix: updates

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,27 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "recharts",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-select",
+      "@radix-ui/react-popover",
+      "@radix-ui/react-navigation-menu",
+      "@radix-ui/react-radio-group",
+      "@radix-ui/react-checkbox",
+      "@tiptap/react",
+      "@tiptap/starter-kit",
+      "date-fns",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.minimize = true;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
